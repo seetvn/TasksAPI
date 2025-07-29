83cfd4bda6a2db6949fbe499fe3ac440d5859fea
seetvn <waiyansteven2002@gmail.com>
2025-07-30 02:01:18 +0400

    commenting routes

--- backend/api/auth/routes.py
+++ backend/api/auth/routes.py
@@ -4,11 +4,13 @@ from auth.utils import create_user, authenticate_user, create_access_token, get_
 
 router = APIRouter(prefix="/auth")
 
+# Creates user and returns an access token.
 @router.post("/signup", response_model=Token)
 async def signup(user: UserCreate):
     res = await create_user(user)
     return res
 
+# Authenticates user and returns an access token.
 @router.post("/token", response_model=Token)
 async def login(user: UserCreate):
     return await authenticate_user(user)
--- backend/api/tasks/routes.py
+++ backend/api/tasks/routes.py
@@ -8,16 +8,17 @@ from models import User
 router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 
+# Creates a new task for the authenticated user.
 @router.post("/", response_model=TaskRead)
 async def create_task(task: TaskCreate, current_user: User = Depends(get_current_user)):
     return await utils.create_task_for_user(task, current_user.id)
 
-
+# Retrieves all tasks belonging to the authenticated user.
 @router.get("/", response_model=List[TaskRead])
 async def get_tasks(current_user: User = Depends(get_current_user)):
     return await utils.get_user_tasks(current_user.id)
 
-
+# Retrieves a specific task by its ID for the authenticated user.
 @router.get("/{task_id}", response_model=TaskRead)
 async def get_task(task_id: int, current_user: User = Depends(get_current_user)):
     task = await utils.get_user_task_by_id(task_id, current_user.id)
@@ -25,7 +26,7 @@ async def get_task(task_id: int, current_user: User = Depends(get_current_user))
         raise HTTPException(status_code=404, detail="Task not found")
     return task
 
-
+# Updates the status of a specific task for the authenticated user.
 @router.put("/{task_id}", response_model=TaskRead)
 async def update_task(task_id: int, update: TaskUpdate, current_user: User = Depends(get_current_user)):
     if update.status is None:
@@ -38,8 +39,7 @@ async def update_task(task_id: int, update: TaskUpdate, current_user: User = Dep
     print("Task updated:", task)
     return task
 
-
-
+# Deletes a specific task for the authenticated user.
 @router.delete("/{task_id}")
 async def delete_task(task_id: int, current_user: User = Depends(get_current_user)):
     success = await utils.delete_user_task(task_id, current_user.id)
